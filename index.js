9a7fda5bdaefe677481e1377fef35b9dac354712
Bernardo <bernardpena@gmail.com>
2024-02-29 01:26:50 -0300

    Filtrando valores negativos

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-let precio = 400000
-let cantidad = 0
+let precio = 400000;
+let cantidad = 0;
 
-// precioSpan = document.querySelector(".precio-inicial");
-// precioSpan.innerHTML = precio
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
 
-// let resta = document.getElementById('resta');
-// resta.disable = true
+function agregar(valuePar) {
+  document.getElementById("cantidad").value;
+  if (valuePar.value == "suma") {
+    cantidad--;
+  } else {
+    cantidad++;
+  }
 
+  document.getElementById("cantidad").textContent = cantidad;
+  total = precio * cantidad;
 
-
-function agregar(valuePar){
-    document.getElementById('cantidad').value;
-    if(valuePar.value == 'suma'){
-        cantidad--;
-    } else {
-        cantidad++;
-    }
-    document.getElementById('cantidad').textContent = cantidad;
-    total = precio * cantidad
-    document.getElementById('valor-total').textContent = total
+  document.getElementById("valor-total").textContent = total;
+  console.log(cantidad + " " + total + " sumando");
 }
 
-function restar(valuePar){
-    document.getElementById('cantidad').value;
-    if(valuePar.value == 'resta'){
-        cantidad++;
-    } else {
-        cantidad--;
-    }
-    document.getElementById('cantidad').textContent = cantidad;
+function restar(valuePar) {
+  document.getElementById("cantidad").value;
+  if (valuePar.value == "resta") {
+    cantidad++;
+  } else {
+    cantidad--;
+  }
+  document.getElementById("cantidad").textContent = cantidad;
 
-    if (cantidad == 0) {
-        // total = 0
-        console.log(cantidad == 0)
-        resta = document.querySelector('resta')
-        resta.innerHTML = button.enable = false
-
-    } else{
-        total = (precio * cantidad)
-        
-    }
-    document.getElementById('valor-total').textContent = total
+  if (cantidad <= 0) {
+    total = 0;
+    cantidad = 0;
+    document.getElementById("valor-total").textContent = total;
+    document.getElementById("cantidad").textContent = 0;
+  } else {
+    total = precio * cantidad;
+    console.log(cantidad + " " + total + " restando");
+  }
+  document.getElementById("valor-total").textContent = total;
 }
